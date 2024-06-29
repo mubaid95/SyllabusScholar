4a44512f46c7d31fd9a029c7f5f34e47d0e2bd45
mubaid95 <145324768+mubaid95@users.noreply.github.com>
2024-06-29 15:01:47 +0530

    Update myconnection.py

--- dbpart/myconnection.py
+++ dbpart/myconnection.py
@@ -1,17 +1,15 @@
 import psycopg2
 from psycopg2 import Error
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 def connect():
     try:
         db_config = {
-            'dbname': os.getenv('DB_NAME'),
-            'user': os.getenv('DB_USER'),
-            'password': os.getenv('DB_PASSWORD'),
-            'host': os.getenv('DB_HOST'),
-            'port': os.getenv('DB_PORT')
+            'dbname': st.secrets["database"]["DB_NAME"],
+            'user': st.secrets["database"]["DB_USER"],
+            'password': st.secrets["database"]["DB_PASSWORD"],
+            'host': st.secrets["database"]["DB_HOST"],
+            'port': st.secrets["database"]["DB_PORT"]
         }
         conn = psycopg2.connect(**db_config)
         return conn
